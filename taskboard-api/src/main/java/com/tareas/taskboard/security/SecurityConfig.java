7b3e7ea2dd173b6e023d2309ef02f8535fbdf694
Alberto Sanchez <albertosanchez15111@gmail.com>
2026-05-22 19:02:09 +0200

    creada clase Securityconfig para la autorizacion de poder entrar en rutas sin estar logueado

---
+++ taskboard-api/src/main/java/com/tareas/taskboard/security/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.tareas.taskboard.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(csrf -> csrf.disable())
+        .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authorizeHttpRequests(auth -> auth
+            .requestMatchers("/api/health", "/api/auth/**").permitAll()
+            .anyRequest().authenticated()
+        );
+        
+        return http.build();
+    }
+    
+}
